0d9d74764ba518a736be335c2450f8ecd2fcf41b
JOnathan Daniel <gcagrad19@gmail.com>
2021-08-23 14:29:52 -0400

    Added in File Serving

--- server/index.js
+++ server/index.js
@@ -4,40 +4,67 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
 
-let restaurantData = [];
+let restaurantData = {};
+/*
+{
+    roomCode1: [],
+    roomCode2: []
+}
+*/
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-    res.send(restaurantData);
+function findData(roomCode) {
+    if (roomCode in restaurantData) {
+        return restaurantData[roomCode];
+    } else {
+        restaurantData[roomCode] = [];
+        return [];
+    }
+}
+
+app.get("/api/:roomCode", (req, res) => {
+    roomData = findData(req.params.roomCode);
+    console.log(req.params.roomCode);
+    console.log(roomData);
+    console.log(restaurantData);
+    res.send(roomData);
 });
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
-app.post("/api", (req, res) => {
+app.post("/api/:roomCode", (req, res) => {
+    console.log(req.params.roomCode);
+    console.log(restaurantData);
+    console.log(restaurantData[req.params.roomCode]);
     let newBusiness = req.body;
 
-    if (!restaurantData.find((element) => newBusiness.name === element.name)) {
-        restaurantData.push(newBusiness);
+    if (
+        !restaurantData[req.params.roomCode].find(
+            (element) => newBusiness.name === element.name
+        )
+    ) {
+        restaurantData[req.params.roomCode].push(newBusiness);
     }
 
-    res.send(restaurantData);
+    res.send(restaurantData[req.params.roomCode]);
 });
 
-app.put("/api/:businessName/:newCount", (req, res) => {
+app.put("/api/:roomCode/:businessName/:newCount", (req, res) => {
     console.log("PUT RUNNING");
     console.log(req.params);
-    let restaurantToUpdate = restaurantData.find(
+    console.log(restaurantData);
+    let restaurantToUpdate = restaurantData[req.params.roomCode].find(
         (element) => element.name === req.params.businessName
     );
 
     if (restaurantToUpdate) {
         restaurantToUpdate.count = req.params.newCount;
-        res.send(restaurantData);
+        res.send(restaurantData[req.params.roomCode]);
     } else {
         res.status(404).send(
             JSON.stringify({ message: "Restaraunt does not exist" })
@@ -45,15 +72,16 @@ app.put("/api/:businessName/:newCount", (req, res) => {
     }
 });
 
-app.delete("/api/:businessName", (req, res) => {
+app.delete("/api/:roomCode/:businessName", (req, res) => {
     console.log(req.params.businessName);
 
-    restaurantData = restaurantData.filter((element) => {
+    restaurantData[req.params.roomCode] = restaurantData[
+        req.params.roomCode
+    ].filter((element) => {
         return element.name !== req.params.businessName;
     });
 
-    console.log(JSON.stringify(restaurantData));
-    res.send(restaurantData);
+    res.send(restaurantData[req.params.roomCode]);
 });
 
 app.listen(port, () => {
