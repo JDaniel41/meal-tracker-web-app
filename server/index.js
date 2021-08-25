9b527a01b188173ec9c210fdbbb6e9a8010c493f
JOnathan Daniel <gcagrad19@gmail.com>
2021-08-25 16:35:59 -0400

    Regenerated Password

--- server/index.js
+++ server/index.js
@@ -6,15 +6,15 @@ const port = process.env.PORT || 3001;
 
 const roomCodeRouter = require("./routes/roomCode");
 
-//app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.use(express.json());
 
 app.use("/api/:roomCode", roomCodeRouter);
 
-/*app.get("*", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});*/
+});
 
 app.listen(port, () => {
     console.log(`Example app listneing at http://localhost:${port}`);
--- server/mongo/Connection.js
+++ server/mongo/Connection.js
@@ -4,13 +4,17 @@ class Connection {
     static async open() {
         if (this.client) return this.client;
 
-        this.client = await MongoClient.connect(this.url);
-        return this.client;
+        try {
+            this.client = await MongoClient.connect(this.url);
+            return this.client;
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
     }
 }
 
 Connection.db = null;
-Connection.url =
-    "mongodb+srv://nodeJsUser:QwEK7DxwHHrAaphU@cluster0.ilevg.mongodb.net/restarauntData?retryWrites=true&w=majority";
+Connection.url = `mongodb+srv://nodeJsUser:${process.env.MONGO_PASSWORD}@cluster0.ilevg.mongodb.net/restarauntData?retryWrites=true&w=majority`;
 
 module.exports = { Connection };
